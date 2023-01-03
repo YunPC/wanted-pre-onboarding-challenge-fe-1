0018ec771f7d8d158d3beb8c79329bc625627123
Daeun Yun <daeun.yun@publy.co>
2023-01-03 12:56:33 +0900

    ✨ 회원가입 성공 시 로컬 스토리지에 토큰 저장

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -1,19 +1,18 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import axios from "axios";
+import * as React from "react";
+import { useState } from "react";
+import TokenLocalStorage from "../../utils/localStorage/tokenLocalStorage";
 
 function Copyright(props: any) {
   return (
@@ -46,7 +45,10 @@ export default function SignUp() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     axios
-      .post(
+      .post<{
+        message: string;
+        token: string;
+      }>(
         "http://localhost:8080/users/create",
         {
           ...signUpData,
@@ -54,7 +56,8 @@ export default function SignUp() {
         { withCredentials: true }
       )
       .then((value) => {
-        console.log("value");
+        const tokenStorage = new TokenLocalStorage();
+        tokenStorage.setToken(value.data.token);
       });
   };
 

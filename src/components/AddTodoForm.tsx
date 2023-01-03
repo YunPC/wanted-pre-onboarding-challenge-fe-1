749e9aedb34e283cc6399bc30aad05eddf27d52f
Daeun Yun <daeun.yun@publy.co>
2023-01-03 16:20:19 +0900

    ✨ 할 일 입력 후 add 버튼 누를 시 서버에 저장

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,17 +1,51 @@
 import { InputBase, Button } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+import TokenLocalStorage from "../../utils/localStorage/tokenLocalStorage";
 
 function AddTodoForm() {
+  const [ToDo, setTodo] = useState({ title: "", content: "" });
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const tokenStorage = new TokenLocalStorage();
+    axios
+      .post(
+        "http://localhost:8080/todos",
+        {
+          ...ToDo,
+        },
+        { headers: { Authorization: tokenStorage.getToken() } }
+      )
+      .then((value) => {
+        console.log("value", value);
+      });
+  };
   return (
     <div>
-      <form style={{ display: "flex", margin: 10 }}>
+      <form onSubmit={onSubmit} style={{ display: "flex", margin: 10 }}>
         <InputBase
           sx={{ ml: 1, flex: 1 }}
-          placeholder="TODO"
+          placeholder="Title"
           inputProps={{
-            "aria-label": "Description",
+            "aria-label": "Todo Title",
           }}
           style={{ width: "90%" }}
           required
+          onChange={(e) => {
+            setTodo((prevTodo) => ({ ...prevTodo, title: e.target.value }));
+          }}
+        />
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Content"
+          inputProps={{
+            "aria-label": "Todo Content",
+          }}
+          style={{ width: "90%" }}
+          required
+          onChange={(e) => {
+            setTodo((prevTodo) => ({ ...prevTodo, content: e.target.value }));
+          }}
         />
         <Button type="submit" variant="text" style={{ width: "10%" }}>
           Add

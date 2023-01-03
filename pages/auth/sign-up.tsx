9dd25ea63ef228f109ec5a9d63d07c744c9e9e23
Daeun Yun <daeun.yun@publy.co>
2023-01-03 12:12:39 +0900

    🦺 회원가입 유효성 추가

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -12,6 +12,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
 
 function Copyright(props: any) {
   return (
@@ -34,14 +35,31 @@ function Copyright(props: any) {
 const theme = createTheme();
 
 export default function SignUp() {
+  const validateEmail = (mail: string) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return true;
+    }
+    return false;
+  };
+  const validatePassword = (password: string) => password.length >= 8;
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
   };
+
+  const [signUpData, setSignUpData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPassWordError] = useState(false);
+
+  const canSubmit =
+    !!signUpData.email &&
+    !!signUpData.password &&
+    !emailError &&
+    !passwordError;
 
   return (
     <ThemeProvider theme={theme}>
@@ -63,80 +81,72 @@ export default function SignUp() {
           </Typography>
           <Box
             component="form"
-            noValidate
             onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
+            noValidate
+            sx={{ mt: 1 }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              error={emailError}
+              helperText={emailError && "이메일 형식이 올바르지 않습니다."}
+              onChange={(e) => {
+                setSignUpData((prevSignUpData) => ({
+                  ...prevSignUpData,
+                  email: e.target.value,
+                }));
+                setEmailError(!validateEmail(e.target.value));
+              }}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              error={passwordError}
+              helperText={passwordError && "비밀번호 형식이 올바르지 않습니다."}
+              onChange={(e) => {
+                setSignUpData((prevSignUpData) => ({
+                  ...prevSignUpData,
+                  password: e.target.value,
+                }));
+                setPassWordError(!validatePassword(e.target.value));
+              }}
+            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              disabled={!canSubmit}
             >
               Sign Up
             </Button>
-            <Grid container justifyContent="flex-end">
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  Already have an account? Sign in
+                  {"Do have an account? Sign In"}
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
+        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );

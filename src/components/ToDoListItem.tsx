1eeeecaebaa2c4d4b6dd727cd611cfc8cc0a270f
Daeun Yun <daeun.yun@publy.co>
2023-01-11 18:37:26 +0900

    ✨ ToDo 삭제 시 사용자에게 한번 더 확인

--- src/components/ToDoListItem.tsx
+++ src/components/ToDoListItem.tsx
@@ -8,7 +8,9 @@ import {
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import TokenLocalStorage from "../../utils/localStorage/tokenLocalStorage";
+import AlertDialog from "./AlertDialog";
 
 interface ToDo {
   title: string;
@@ -28,6 +30,8 @@ function ToDoListItem({ toDo, setToDos }: ToDoListItemProps) {
   const [editMode, setEditMode] = useState(false);
   const [title, setTitle] = useState(toDo.title);
   const tokenStorage = new TokenLocalStorage();
+  const [isModalOpened, setIsModalOpened] = useState(false);
+
   return (
     <>
       {editMode && (
@@ -83,11 +87,46 @@ function ToDoListItem({ toDo, setToDos }: ToDoListItemProps) {
           >
             <Edit fontSize="small" />
           </IconButton>
-          <IconButton color="secondary" aria-label="Delete">
+          <IconButton
+            onClick={() => {
+              setIsModalOpened(true);
+            }}
+            color="secondary"
+            aria-label="Delete"
+          >
             <Clear fontSize="small" />
           </IconButton>
         </ListItemButton>
       )}
+      <AlertDialog
+        open={isModalOpened}
+        onClose={() => {
+          setIsModalOpened(false);
+        }}
+        title="할 일 삭제"
+        content="할 일을 정말 삭제하시겠습니까?"
+        onCancle={() => {
+          setIsModalOpened(false);
+        }}
+        onConfirm={() => {
+          const loadingToastId = toast.loading("삭제 중입니다.");
+          axios
+            .delete(`http://localhost:8080/todos/${toDo.id}`, {
+              headers: { Authorization: tokenStorage.getToken() },
+            })
+            .then(() => {
+              toast.update(loadingToastId, {
+                render: "삭제가 완료 되었습니다!",
+                type: "success",
+                isLoading: false,
+                autoClose: 2000,
+              });
+              setToDos((prevToDos) =>
+                prevToDos.filter(({ id }) => id !== toDo.id)
+              );
+            });
+        }}
+      />
     </>
   );
 }
